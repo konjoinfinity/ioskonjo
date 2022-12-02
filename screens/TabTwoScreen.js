e243293d700c252dd7f64695f0cb36dd735046ec
Wesley Scholl <clients@konjoweb.com>
2022-12-02 17:50:10 -0500

    updated styling to prevent scroll list from pushing button off screen

--- screens/TabTwoScreen.js
+++ screens/TabTwoScreen.js
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View, TouchableHighlight, ScrollView } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, SafeAreaView } from 'react-native';
+import { useState, useEffect } from 'react';
 import { SwipeListView } from 'react-native-swipe-list-view';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import Dialog from "react-native-dialog";
+import { Button } from '@rneui/themed';
 
 export default function TabTwoScreen() {
   const [visible, setVisible] = useState(false);
+  const [visible1, setVisible1] = useState(false);
   const [text, setText] = useState("");
   const [textref, setTextref] = useState("");
-    var [list, setlist] = useState(Array(5)
-    .fill("")
-    .map((_, i) => ({ key: `${i}`, text: `item #${i}` })));
+    var [list, setlist] = useState(Array(5).fill("").map((_, i) => ({ key: `${i}`, text: `Item #${i}` })));
 
     const deleteItem = (todel) => {
       var filteredArray = list.filter(item => {
@@ -20,52 +20,71 @@ export default function TabTwoScreen() {
     }
 
     const editItem = (toedit) => {
-      
-      // alert(`Deleted" ${todel}`)
-      // setlist(filteredArray)
-      console.log(toEdit[0].text)
+      var toEdit = list.filter(item => {
+        return (item.key == toedit)})
       setVisible(true)
       setText(toEdit)
       setTextref(toEdit)
     }
 
-    const handleCancel = () => {
-      setVisible(false);
+    const addItem = () => {
+      setVisible1(true)
+    }
+
+    const handleCancel = (which) => {
+      which == "edit" ? setVisible(false) : setVisible1(false)
     };
 
-    const handleEdit = (edit) => {
-      setText(edit)
-      console.log(text)
+    const handleInput = (input) => {
+      setText(input)
     };
   
-    const handleSubmit = () => {
-      // list[textref[0].key] = 
-      
+    const handleSubmit = (which) => {
+      if (which == "edit") {
+      list[textref[0].key] = {"key": `${textref[0].key}`, "text": `${text}`}
       setVisible(false);
+      } else {
+        list.push({"key": `${list.length}`, "text": `${text}`})
+        setVisible1(false)
+      }
+      
     };
 
+  
+
   return (
-    <View>
+    <SafeAreaView>
       <Dialog.Container visible={visible}>
-      <Dialog.Title>Account delete</Dialog.Title>
+      <Dialog.Title>Edit</Dialog.Title>
       <Dialog.Description>
-        Do you want to delete this account? You cannot undo this action.
+        Modify Note
       </Dialog.Description>
-      <Dialog.Input id="edit" onChangeText={handleEdit} defaultValue={text !== "" ? (text[0].text) : ("")}>
+      <Dialog.Input id="input" onChangeText={handleInput} defaultValue={text !== "" ? (text[0].text) : ("")}>
+      </Dialog.Input>
+      <Dialog.Button label="Cancel" onPress={()=>handleCancel("edit")} />
+        <Dialog.Button label="Submit" onPress={()=>handleSubmit("edit")} />
+    </Dialog.Container>
+    <Dialog.Container visible={visible1}>
+      <Dialog.Title>Add</Dialog.Title>
+      <Dialog.Description>
+        Add Note
+      </Dialog.Description>
+      <Dialog.Input id="input" onChangeText={handleInput} defaultValue={""}>
       </Dialog.Input>
       <Dialog.Button label="Cancel" onPress={handleCancel} />
-        <Dialog.Button label="Submit" onPress={handleSubmit} />
+        <Dialog.Button label="Submit" onPress={()=>handleSubmit("add")} />
     </Dialog.Container>
       <SwipeListView
+            style={{maxHeight: "90%"}}
             data={list}
             renderItem={ (data, rowMap) => (
                 <View style={styles.rowFront}>
-                    <Text>Item {data.item.text}</Text>
+                    <Text>{data.item.text}</Text>
                 </View>
             )}
             renderHiddenItem={ (data, rowMap) => (
               <View style={{display: "flex", flexDirection:"row"}}>
-              <TouchableHighlight onPress={() => editItem(data.item.key)}
+              <TouchableHighlight onPress={() => {editItem(data.item.key); rowMap[data.item.key].closeRow()}}
                     style={styles.rowBack2}>
               <Text style={{paddingLeft: 15, color: "white", width: "50%"}}>Edit</Text></TouchableHighlight>
                     <TouchableHighlight onPress={() => deleteItem(data.item.key)}
@@ -73,10 +92,12 @@ export default function TabTwoScreen() {
                       <Text style={{paddingRight: 15, color: "white", width: "50%"}}>Delete</Text></TouchableHighlight>
                       </View>
             )}
+            
             leftOpenValue={100}
             rightOpenValue={-120}
         />
-</View>
+<Button raised={true} title='Add New' color={"success"} onPress={addItem}><Ionicon color={"white"} size={40} name="add-outline"></Ionicon></Button>
+</SafeAreaView>
   );
             }
 
@@ -102,7 +123,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#eeeeee',
     flex: 1,
     flexDirection: "column",
-    padding: 30
+    padding: 30,
+    alignItems: "center"
 },
 rowBack: {
   backgroundColor: "#FC3D39",
