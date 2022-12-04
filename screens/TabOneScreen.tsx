ecdc660d243af40bf0bf46a9714fac65ba38cb96
Wesley Scholl <clients@konjoweb.com>
2022-12-03 19:26:16 -0500

    adding map feature for homescreen - users

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,12 +1,101 @@
-import { StyleSheet, Image } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Image, ScrollView } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Button, Avatar } from "react-native-elements"
+
+
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [users, setUsers] = useState("");
+
+  const getData = async () => {
+    await fetch("https://638bd25dd2fc4a058a4e6502.mockapi.io/user", {
+      method: "GET"
+    })
+      .then(res => res.text())
+      .then(res => {
+        setUsers(res);
+      }).catch(error => {
+        console.log(String(error))
+      });
+    }
+
+  useEffect(() => {
+    getData();
+  }, []);
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Konjo</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <ScrollView>
+      <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            borderRadius: 5,
+            alignItems: 'center',
+            marginHorizontal: 10,
+            height: 250,
+            marginBottom: 10,
+          }}
+        >
+          <View style={{ flex: 3, flexDirection: 'row' }}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar
+                size={145}
+                source={{
+                  uri: 'https://randomuser.me/api/portraits/men/1.jpg',
+                }}
+                activeOpacity={0.7}
+                avatarStyle={{ borderRadius: 145 / 2 }}
+                overlayContainerStyle={{ backgroundColor: 'transparent' }}
+              />
+            </View>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  marginTop: 10,
+                  justifyContent: 'center',
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 25,
+                    color: "white",
+                    marginLeft: -15,
+                  }}
+                >
+                  Paul Allen
+                </Text>
+              </View>
+            </View>
+          </View>
+          <Button title={"Profile"}></Button>
+          <View
+            style={{
+              width: 300,
+              borderWidth: 0.5,
+              borderColor: 'rgba(222, 223, 226, 1)',
+              marginHorizontal: 20,
+              height: 1,
+              marginVertical: 10,
+            }}
+          />
+        </View>
+        </ScrollView>
     </View>
   );
 }
