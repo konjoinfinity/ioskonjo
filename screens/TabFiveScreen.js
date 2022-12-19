de3e9bd49584d6a064a2152c584516d678633d21
Wesley Scholl <clients@konjoweb.com>
2022-12-19 12:32:42 -0500

    added more snow type data, up to marginal snow

--- screens/TabFiveScreen.js
+++ screens/TabFiveScreen.js
@@ -12,7 +12,7 @@ export function Card({navigation, cardData}){
              <Text style={{fontSize: Dimensions.get('window').height * 0.03, fontWeight: "bold"}}>{cardData.anum}</Text> 
              <Text style={{fontSize: Dimensions.get('window').height * 0.03, fontWeight: "bold"}}>{cardData.acronymn}</Text>
              </View>
-             <Text style={{ fontSize: Dimensions.get('window').height * 0.028, fontWeight: "bold", padding: 5, marginTop: 10, alignSelf: "center" }}>{cardData.title}</Text>
+             <Text style={{ fontSize: Dimensions.get('window').height * 0.027, fontWeight: "bold", padding: 5, marginTop: 10, alignSelf: "center" }}>{cardData.title}</Text>
                 </TouchableOpacity>
         )
     }
@@ -223,28 +223,280 @@ export default function Home({navigation}) {
             acronymn: "UTA",
             kind: 'Exquisite Snow ❄️',
             title: '"Greatest Snow On Earth"',
-            desc: 'Slogan appears Utah license plates in 1985; Debatable claim ** Said the man from Colorado',
+            desc: 'Slogan appears Utah license plates in 1985; Debatable claim ** Said the man from Colorado.',
             backgroundColor: "#5B9BD5"
         },
+        {
+            key: 23,
+            anum: "24",
+            acronymn: "PP",
+            kind: 'Ideal Snow ❄️',
+            title: 'Packed Powder',
+            desc: 'New snow compacted by grooming or skier traffic;  Delightful!',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 24,
+            anum: "25",
+            acronymn: "CY",
+            kind: 'Ideal Snow ❄️',
+            title: 'Corduroy',
+            desc: 'Freshly groomed snow exhibiting ridges or ribbed surface, soft much like the cloth.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 25,
+            anum: "26",
+            acronymn: "C",
+            kind: 'Ideal Snow ❄️',
+            title: 'Corn',
+            desc: 'Course niblets of snow forming during brief period between frozen and slushy surface.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 26,
+            anum: "27",
+            acronymn: "CH",
+            kind: 'Ideal Snow ❄️',
+            title: 'Chaulk',
+            desc: 'Wind-driven snow that is smoothly distributed, lightly packed and delightful.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 27,
+            anum: "28",
+            acronymn: "BU",
+            kind: 'Ideal Snow ❄️',
+            title: 'Butter (Cream)',
+            desc: 'Smooth and silky like the dairy product, but can change quickly.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 28,
+            anum: "29",
+            acronymn: "WS",
+            kind: 'Ideal Snow ❄️',
+            title: 'White Suede',
+            desc: 'Smooth hard snow w/ a mysterious surface that allows for easy, smooth carving;  Nice!',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 29,
+            anum: "30",
+            acronymn: "SU",
+            kind: 'Ideal Snow ❄️',
+            title: 'Sugar',
+            desc: 'Small granules but smooth and sweet like a confection, only colder and quicker.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 30,
+            anum: "31",
+            acronymn: "CO",
+            kind: 'Ideal Snow ❄️',
+            title: 'Chop',
+            desc: 'Cut-up powder with many lines tracted, but still nice & enjoyable.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 31,
+            anum: "32",
+            acronymn: "SK",
+            kind: 'Ideal Snow ❄️',
+            title: 'Skift',
+            desc: 'Very light new snow, in such small amount it will not drift;  Nice.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 32,
+            anum: "33",
+            acronymn: "SD",
+            kind: 'Ideal Snow ❄️',
+            title: 'Soufflé Dure',
+            desc: 'Naturally packed, usually by the wind, firm but lovely snow; Tasty as the name says.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 33,
+            anum: "34",
+            acronymn: "SQ",
+            kind: 'Ideal Snow ❄️',
+            title: 'Squeaky (Screaming Lobsters)',
+            desc: 'Very cold, very dry, emitting a high pitch sound as walked on.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 34,
+            anum: "35",
+            acronymn: "VL",
+            kind: 'Ideal Snow ❄️',
+            title: 'Velvet',
+            desc: 'Smooth hard snow w/ forgiving firm surface that allows for easy, smooth carving; A joy.',
+            backgroundColor: "#8EAADB"
+        },
+        {
+            key: 35,
+            anum: "36",
+            acronymn: "G",
+            kind: 'Good Snow ❄️',
+            title: 'Groomed',
+            desc: '"Farmed" snow that is rolled, smoothed, tilled, manicured into a consistent surface.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 36,
+            anum: "37",
+            acronymn: "SL",
+            kind: 'Good Snow ❄️',
+            title: 'Sluff',
+            desc: 'Loose snow set into motion by a skier; but NOT an avalanche.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 37,
+            anum: "38",
+            acronymn: "FG",
+            kind: 'Good Snow ❄️',
+            title: 'Frozen Granular',
+            desc: 'Cold sugary snow whose granules have frozen together.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 38,
+            anum: "39",
+            acronymn: "GR",
+            kind: 'Good Snow ❄️',
+            title: 'Granular',
+            desc: 'Like Frozen Granular, but with larger-sized loose granules.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 39,
+            anum: "40",
+            acronymn: "SH",
+            kind: 'Good Snow ❄️',
+            title: 'Surface Hoar',
+            desc: 'Needle-like shaped crystals of frost, forms on clear nights when snow is colder than air.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 40,
+            anum: "41",
+            acronymn: "FF",
+            kind: 'Good Snow ❄️',
+            title: 'Fluffernutter',
+            desc: '3-6 inches of fluff on top of a chunky sub-surface, alternating the ride as smooth and not.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 41,
+            anum: "42",
+            acronymn: "WN",
+            kind: 'Good Snow ❄️',
+            title: 'Windbuff',
+            desc: 'Fine snow that is redistributed by the wind and consolidated.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 42,
+            anum: "43",
+            acronymn: "MM",
+            kind: 'Good Snow ❄️',
+            title: 'Machine-Made (Man-Made)',
+            desc: 'Snow manufactured by water, compressed air, snow guns; Dense.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 43,
+            anum: "44",
+            acronymn: "TO",
+            kind: 'Good Snow ❄️',
+            title: 'Tracked Out',
+            desc: 'Late-day powder, very cut-up and uneven, you will be bounced around; need to groom.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 44,
+            anum: "45",
+            acronymn: "B",
+            kind: 'Good Snow ❄️',
+            title: 'Base',
+            desc: 'Completely consolidated snow; Reported depth of snow cover by ski area.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 45,
+            anum: "46",
+            acronymn: "FS",
+            kind: 'Good Snow ❄️',
+            title: 'Flour-sifter Snow',
+            desc: 'A Montana term for fine small-flaked snow.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 46,
+            anum: "47",
+            acronymn: "LP",
+            kind: 'Marginal Snow ❄️',
+            title: 'Loud Powder',
+            desc: 'Euphemism for "Ice" or "Blue" or extreme hardpack; Noisy snow.',
+            backgroundColor: "#DEEAF6"
+        },
+        {
+            key: 47,
+            anum: "48",
+            acronymn: "LG",
+            kind: 'Good Snow ❄️',
+            title: 'Loose Granular',
+            desc: 'Smaller-sized loose granules, or pellets, of snow, uncohesive.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 48,
+            anum: "49",
+            acronymn: "HP",
+            kind: 'Good Snow ❄️',
+            title: 'Hardpack',
+            desc: 'Firmly consolidated and compressed old snow; slick and fast; Bring your edges...',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 49,
+            anum: "50",
+            acronymn: "GP",
+            kind: 'Good Snow ❄️',
+            title: 'Graupel',
+            desc: '"Soft hail" occurs when flakes form a lumpy mass, also a heavily rimed particle.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 50,
+            anum: "51",
+            acronymn: "N",
+            kind: 'Good Snow ❄️',
+            title: 'Neve',
+            desc: 'Young granular snow, partly melted, refrozen and compacted; At times, soft and nice.',
+            backgroundColor: "#9CC3E5"
+        },
+        {
+            key: 51,
+            anum: "52",
+            acronymn: "SP",
+            kind: 'Marginal Snow ❄️',
+            title: 'Sposh',
+            desc: "Many years ago, a New Englander's term for a soft slushy snow",
+            backgroundColor: "#DEEAF6"
+        },
+        {
+            key: 52,
+            anum: "53",
+            acronymn: "MC",
+            kind: 'Marginal Snow ❄️',
+            title: 'Marble Crust',
+            desc: "Extremely hard icy snow surface, usually found in small, round patches; Like the stone.",
+            backgroundColor: "#DEEAF6"
+        },
 
-        // {
-        //     key: 9,
-        //     anum: "8",
-        //     acronymn: "HS",
-        //     kind: 'Good Snow ❄️',
-        //     title: 'Groomed',
-        //     desc: '"Farmed" snow that is rolled, smoothed, tilled, manicured into a consistent surface.',
-        //     backgroundColor: "#6A9ED0"
-        // },
-        // {
-        //     key: 10,
-        //     anum: "8",
-        //     acronymn: "HS",
-        //     kind: 'Good Snow ❄️',
-        //     title: 'Windbuff',
-        //     desc: 'Fine snow that is redistributed by the wind and consolidated.',
-        //     backgroundColor: "#6A9ED0"
-        // },
         // {
         //     key: 11,
         //     anum: "8",
