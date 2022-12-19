abebff499689066785bfbec150e225fc65e76105
Wesley Scholl <clients@konjoweb.com>
2022-12-19 18:20:37 -0500

    updated input style

--- screens/TabFiveScreen.js
+++ screens/TabFiveScreen.js
@@ -22,8 +22,7 @@ export default function TabFiveScreen({ navigation }) {
   const handleChange = (search) => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
     search = search.trim().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "").toLowerCase()
-    setSearch({ search });
-    console.log(search)
+    setSearch({ search }); 
     cardSearch = cards.filter(function (card) { return card.title.toLowerCase().match(search) })
 }
 
@@ -32,7 +31,7 @@ export default function TabFiveScreen({ navigation }) {
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
       <ScrollView contentContainerStyle={{paddingLeft: 12, height: Dimensions.get('window').height * 0.3, width: Dimensions.get('window').width * 1, alignItems: "center", justifyContent:"flex-start", display: "flex", flexDirection: "row", flexWrap: "wrap"}}>{search.length !== 0 && cardSearch.length < 20 ? cardSearch.map((card) => 
       (<TouchableOpacity onPress={() => navigation.navigate('Modal', {cardData: card})}style={{backgroundColor: card.backgroundColor, width: Dimensions.get('window').width * 0.23, height: Dimensions.get('window').width * 0.18, margin: 0.5}} key={card.key}><Text style={{color: "black", fontSize: Dimensions.get('window').width * 0.035,  padding: 10, }}>{card.title}</Text></TouchableOpacity>)) : ("")}</ScrollView>
-      <TextInput color="#5B9BD5" textAlignVertical="center" style={{width: Dimensions.get('window').width * 0.9,  alignSelf: "center", marginBottom: Dimensions.get('window').width * 0.6  }} placeholder='❅❆❄ Which snow? ❅❆❄' name="search" id="search" onChangeText={handleChange} ref={(input) => { textInput = input; }}/>
+      <TextInput variant="outlined" color="#5B9BD5" textAlignVertical="center" style={{width: Dimensions.get('window').width * 0.9,  alignSelf: "center", marginBottom: Dimensions.get('window').width * 0.6  }} placeholder='❅❆❄ Which snow? ❅❆❄' name="search" id="search" onChangeText={handleChange} ref={(input) => { textInput = input; }}/>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
 
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
