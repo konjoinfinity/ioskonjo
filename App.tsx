6c898a31974b7eecfefbd1c575dc2e3bd9cb80f5
Wesley Scholl <clients@konjoweb.com>
2023-01-03 16:44:07 -0500

    updated 3 second timer for splash

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
 import AnimatedSplash from "react-native-animated-splash-screen";
@@ -11,6 +11,13 @@ import AnimatedSplash from "react-native-animated-splash-screen";
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [showSplash, setShowSplash] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplash(true)
+    }, 3000)
+  }, [])
 
   if (!isLoadingComplete) {
     return null
@@ -18,7 +25,7 @@ export default function App() {
     return (
       <AnimatedSplash
       translucent={true}
-      isLoaded={isLoadingComplete}
+      isLoaded={showSplash}
       logoImage={require("./assets/images/snowlogo.png")}
       backgroundColor={"#000000"}
       logoHeight={200}
