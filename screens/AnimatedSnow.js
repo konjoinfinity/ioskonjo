d86ecb4626c33c1d42309535240c33c090996045
Wesley Scholl <clients@konjoweb.com>
2023-01-12 16:01:02 -0500

    added conditional for Android splash screen, removed snow to test fix for crashing issue

--- screens/AnimatedSnow.js
+++ screens/AnimatedSnow.js
@@ -37,7 +37,7 @@ export function Card({title, color, copy}){
           />)}}
   
       return (
-        <View style={{backgroundColor: "#000", height: height, width: width}}>
+        Platform.OS == "ios" ? <View style={{backgroundColor: "#000", height: height, width: width}}>
         <Animated.View style={{ opacity: fadeAnim, backgroundColor: "#000" }}>
         <View {...props}
           onLayout={(e) => {
@@ -52,8 +52,14 @@ export function Card({title, color, copy}){
                 <AnimatableView animation="bounceInDown" delay={1250} duration={2000}><Card title={"Snow"} copy={"©"} color={"#ECECEC"} /></AnimatableView>          
                 </View>
             </View>
-            </Animated.View>
-            </View>
+          </Animated.View>
+        </View> :
+        <View style={{display: "flex", flexDirection: "row", flexWrap: "wrap", alignItems:"center", paddingTop: Dimensions.get('window').height * 0.3, paddingBottom: Dimensions.get('window').height * 0.3, paddingLeft: Dimensions.get('window').width * 0.04, paddingRight: Dimensions.get('window').width * 0.04, backgroundColor: "#000"}}>
+        <AnimatableView animation="bounceInDown" delay={500} duration={2000}><Card title={"Periodic"} color={"#5B9BD5"} /></AnimatableView>      
+        <AnimatableView animation="bounceInDown" delay={750} duration={2000}><Card title={"Table"} color={"#DEEAF6"} /></AnimatableView>  
+        <AnimatableView animation="bounceInDown" delay={1000} duration={2000}><Card title={"of"} color={"#FFD965"} /></AnimatableView>  
+        <AnimatableView animation="bounceInDown" delay={1250} duration={2000}><Card title={"Snow"} copy={"©"} color={"#ECECEC"} /></AnimatableView>          
+        </View>
       )
     }
 
