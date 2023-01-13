f4d54033ef8a9951d23e2dcbf16cceb607950b74
Wesley Scholl <clients@konjoweb.com>
2023-01-12 21:18:08 -0500

    testing find snow method, writing to asyncstorage for persisting found snow data

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,23 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Platform, StyleSheet, Dimensions, Button, Pressable } from 'react-native';
+import { Platform, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
 import * as Haptics from 'expo-haptics';
-import { useTheme } from '@react-navigation/native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const foundkey = "found";
 
 export default function ModalScreen({ route }) {
   const [foundSnow, setFoundSnow] = useState("#fff");
-  const { colors } = useTheme();
+  const [found, setFound] = useState([]);
   const {cardData} = route.params;
   
   useEffect(() => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+    getFound();
+    // var filtered = found.filter(function(snow) { return snow.snow != route.params.loggy.log}); 
   }, [])
 
-  const found = () => {
-    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
-    foundSnow == "#fff" ? setFoundSnow("#94C68A") : setFoundSnow("#fff")
+  const getFound = async() => {
+      // try {
+      //   await AsyncStorage.removeItem(foundkey)
+      // } catch(e) {
+      // }
+      // console.log('Done.')
+    try {
+      await AsyncStorage.getItem(foundkey, (error, result) => {
+        result !== null && result !== "[]" && result !== undefined ? 
+        setFound(JSON.parse(result)) : setFound([]);
+        console.log("Get")
+        console.log(result)
+      });
+      console.log(found)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const foundThis = async(snowtype) => {
+    try {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+      foundSnow == "#fff" ? setFoundSnow("#94C68A") : setFoundSnow("#fff")
+      var newFound = found;
+      console.log(found)
+      if(foundSnow == "#fff") {
+        newFound.unshift({ snowType: snowtype, dateFound: new Date() })
+        console.log(newFound)
+        setFound(newFound)
+        console.log(found)
+        await AsyncStorage.setItem(foundkey, JSON.stringify(newFound));
+        console.log(found)
+      } else {
+        newFound.shift();
+        console.log(newFound)
+        setFound(newFound)
+        console.log(found)
+        await AsyncStorage.setItem(foundkey, JSON.stringify(newFound));
+        console.log(found)
+      }
+    } catch(err) {
+      console.log(err)
+    }
   }
 
   return (
@@ -31,7 +74,7 @@ export default function ModalScreen({ route }) {
              </View>
              <Text style={{ fontSize: Dimensions.get('window').height * 0.045, fontWeight: "bold", padding: 10, marginTop: 12, alignSelf: "center", color: "black", textAlign: "center" }}>{cardData.title}</Text>
              <Text style={{ fontSize: Dimensions.get('window').height * 0.03, padding: 10, margin: 10, alignSelf: "center", color: "black"}}>{cardData.desc}</Text>
-             {Platform.OS == "ios" ? <TouchableOpacity style={{alignSelf: "center", width: Dimensions.get('window').width * 0.6, marginTop: Dimensions.get('window').height * 0.025, height: Dimensions.get('window').height * 0.09,
+             <TouchableOpacity style={{alignSelf: "center", maxWidth: Dimensions.get('window').width * 0.8, marginTop: Dimensions.get('window').height * 0.025,
              backgroundColor: foundSnow,
                   shadowColor: 'rgba(200,200,200, 200)', 
                 shadowOffset: { height: 2.5, width: 2.5 }, 
@@ -42,19 +85,8 @@ export default function ModalScreen({ route }) {
                 justifyContent: 'center',
                 alignItems: 'center',
                 flexDirection: 'row',
-                }} onPress={() => found()}><Text style={{textAlign: "center", color: "#000" }}>{`Found ${cardData.title}?`}</Text></TouchableOpacity> :
-                <Pressable style={{alignSelf: "center", width: Dimensions.get('window').width * 0.6, marginTop: Dimensions.get('window').height * 0.025, height: Dimensions.get('window').height * 0.09,
-             backgroundColor: foundSnow,
-                  shadowColor: 'rgba(200,200,200, 200)', 
-                shadowOffset: { height: 2.5, width: 2.5 }, 
-                shadowOpacity: 1, 
-                shadowRadius: 1, 
-                borderRadius: 5,
-                elevation: 2, 
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'row',
-                }} onPress={() => found()}><Text style={{textAlign: "center", color: "#000" }}>{`Found ${cardData.title}?`}</Text></Pressable>}
+                }} onPress={() => foundThis(cardData.title)}><Text style={{textAlign: "center", color: "#000", fontWeight:"600", fontSize: Dimensions.get('window').height * 0.02, padding: 22 }}>{foundSnow == "#fff" ? `Found ${cardData.title}?`: `You Found ${cardData.title}!`}</Text></TouchableOpacity> 
+                {/* <Text style={{ fontSize: Dimensions.get('window').height * 0.03, padding: 10, margin: 10, alignSelf: "center", color: "black"}}>{cardData.desc}</Text> */}
                 </View>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
